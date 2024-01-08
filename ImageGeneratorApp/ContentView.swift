9a76736f31f05e4925b13c6e6b57909a88a43bbd
Fahad9786 <sheikhfahad2005@gmail.com>
2024-01-08 11:55:29 -0500

    Initial Commit

---
+++ ImageGeneratorApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ImageGeneratorApp
+//
+//  Created by Fahad Sheikh on 2024-01-08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ImageGeneratorApp/ImageGeneratorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageGeneratorAppApp.swift
+//  ImageGeneratorApp
+//
+//  Created by Fahad Sheikh on 2024-01-08.
+//
+
+import SwiftUI
+
+@main
+struct ImageGeneratorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
